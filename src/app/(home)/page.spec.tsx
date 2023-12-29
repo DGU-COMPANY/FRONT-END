2f3b64adad836f6be363eb00f2a031d96602097d
DuyTinh2003 <tranduytinh258@gmail.com>
2023-12-29 17:03:10 +0700

    update: config project

--- src/app/(home)/page.spec.tsx
+++ src/app/(home)/page.spec.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from "@testing-library/react";
-import HomePage from "./page";
+import { render, screen } from '@testing-library/react'
+import HomePage from './page'
 
-describe("HomePage", () => {
-  it("renders the correct content", () => {
-    render(<HomePage />);
-    const headingElement = screen.getByText(/this is home page/i);
-    expect(headingElement).toBeInTheDocument();
-  });
-});
+describe('HomePage', () => {
+  it('renders the correct content', () => {
+    render(<HomePage />)
+    const headingElement = screen.getByText(/this is home page/i)
+    expect(headingElement).toBeInTheDocument()
+  })
+})
